c38562af97d98050444e9b1a41180eec620dd554
Pierre-Hugues Husson <phh@phh.me>
2018-08-14 19:33:23 +0200

    First drop_privs (which may fail) and only run thread that might be scheduled before us
    Change-Id: I118fb2d4beedbeecf5d3a8d255929d3be480b923

--- logd/main.cpp
+++ logd/main.cpp
@@ -440,6 +440,12 @@ int main(int argc, char* argv[]) {
         if (fdPmesg < 0) android::prdebug("Failed to open %s\n", proc_kmsg);
     }
 
+    bool auditd =
+        __android_logger_property_get_bool("ro.logd.auditd", BOOL_DEFAULT_TRUE);
+    if (drop_privs(klogd, auditd) != 0) {
+        return EXIT_FAILURE;
+    }
+
     // Reinit Thread
     sem_init(&reinit, 0, 0);
     sem_init(&uidName, 0, 0);
@@ -459,12 +465,6 @@ int main(int argc, char* argv[]) {
             }
         }
         pthread_attr_destroy(&attr);
-    }
-
-    bool auditd =
-        __android_logger_property_get_bool("ro.logd.auditd", BOOL_DEFAULT_TRUE);
-    if (drop_privs(klogd, auditd) != 0) {
-        return EXIT_FAILURE;
     }
 
     // Serves the purpose of managing the last logs times read on a
