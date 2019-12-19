omnirom/android_system_core_old

Store crashing process name in sysprop
As part of Watchdog-triggered rollback logging, it would
be useful to know the service which has caused
updatable_crashing to be set. Store this in a new property
sys.init.updatable_crashing_process_name.

Test: manual
Bug: 138782888
Change-Id: Iffa38b2b746abbb5d823a8d69b7b8d2456836f4e

Former-commit-id: 5d0deb563d252ab41ea9fc0107477a6b738afbb9

init/service.cpp
@@ -326,6 +326,7 @@ void Service::Reap(const siginfo_t& siginfo) {
                                << (boot_completed ? "in 4 minutes" : "before boot completed");
                     // Notifies update_verifier and apexd
                     SetProperty("sys.init.updatable_crashing", "1");
+                    SetProperty("sys.init.updatable_crashing_process_name", name_);
                 }
             }
         } else {

ArrowOS/android_system_core-old

ro.init.updatable_crashing_process_name.
Bug: 146415463
Merged-In: Iffa38b2b746abbb5d823a8d69b7b8d2456836f4e
(cherry picked from commit 6d9f32b849a3282ebb810000fc5af241156eb85d)

init/service.cpp
@@ -387,6 +387,7 @@ void Service::Reap(const siginfo_t& siginfo) {
                     LOG(ERROR) << "updatable process '" << name_ << "' exited 4 times "
                                << (boot_completed ? "in 4 minutes" : "before boot completed");
                     // Notifies update_verifier and apexd
+                    property_set("ro.init.updatable_crashing_process_name", name_);
                     property_set("ro.init.updatable_crashing", "1");
                 }
             }